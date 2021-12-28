e6e0e6fad4851a401553cfe385835b6303e643f9
unknown <widya160499@gmail.com>
2021-12-28 12:22:18 +0800

    update sidebar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -5,14 +5,14 @@
 
     <div class="navbar-collapse collapse">
         <ul class="navbar-nav navbar-align">
-    
+
             <li class="nav-item dropdown">
                 <a class="nav-icon dropdown-toggle d-inline-block d-sm-none" href="#" data-toggle="dropdown">
                     <i class="align-middle" data-feather="settings"></i>
                 </a>
 
                 <a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-toggle="dropdown">
-                <span class="text-dark">Charles Hall</span>
+                    <span class="text-dark">{{ Auth::user()->nama }}</span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right">
                     {{-- <a class="dropdown-item" href="pages-profile.html"><i class="align-middle mr-1" data-feather="user"></i> Profile</a>
@@ -20,12 +20,12 @@
                     <a class="dropdown-item" href="#">Log out</a> --}}
                     <a class="dropdown-item has-icon text-danger" href="{{ route('logout') }}" onclick="event.preventDefault();
                           document.getElementById('logout-form').submit();">
-          <i class="fas fa-sign-out-alt"></i> {{ __('Logout') }}
-        </a>
+                        <i class="fas fa-sign-out-alt"></i> {{ __('Logout') }}
+                    </a>
 
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-          @csrf
-        </form>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
                 </div>
             </li>
         </ul>
