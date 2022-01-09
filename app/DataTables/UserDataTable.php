aa87858fc219a5acdd535372e69bcd935ef5fc13
unknown <widya160499@gmail.com>
2022-01-09 14:25:53 +0800

    update datatable

--- app/DataTables/UserDataTable.php
+++ app/DataTables/UserDataTable.php
@@ -13,8 +13,8 @@ class UserDataTable
             ->addColumn('action', function ($user) use ($level) {
                 return
                     '<div class="btn-group">' .
-                    '<a href="' . route($level . '.edit', $user->id) . '" class="btn btn-warning" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Edit" style="margin-right: 5px" ><i class="menu-icon fa fa-pencil-alt"></i></a>' .
-                    '<a href="' . route($level . '.show', $user->id) . '" class="btn btn-info" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Lihat" style="margin-right: 5px" ><i class="menu-icon fa fa-info"></i></a>' .
+                    '<a href="' . route($level . '.edit', $user->id) . '" class="btn  btn-sm  btn-warning" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Edit" style="margin-right: 5px" ><b> Edit </b></a>' .
+                    '<a href="' . route($level . '.show', $user->id) . '" class="btn  btn-sm btn-info" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Lihat" style="margin-right: 5px" ><b> Lihat</b></a>' .
                     '</div>';
             })->addIndexColumn()->rawColumns(['action'])->make(true);
     }
