9163c0b7953162b1a3fd215ae722774124125eb4
mertayasa <made.b4li@gmail.com>
2022-01-09 17:53:41 +0800

    Fix btn sm anggota kelas

--- app/DataTables/AnggotaKelasDataTable.php
+++ app/DataTables/AnggotaKelasDataTable.php
@@ -16,7 +16,7 @@ class AnggotaKelasDataTable
                 $deleteUrl = "'" . route('anggota_kelas.destroy', $anggota_kelas->id) . "', 'AnggotaKelasDataTable'";
                 return
                     '<div class="btn-group">' .
-                    '<a href="' . 'asd' . '" class="btn btn-primary" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Lihat Profil" style="margin-right: 5px" > <b> Profil </b> </a>' .
+                    '<a href="' . 'asd' . '" class="btn btn-primary btn-sm" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Lihat Profil" style="margin-right: 5px" > <b> Profil </b> </a>' .
                     '<a href="' . route('siswa.show', $anggota_kelas->siswa->id) . '" class="btn btn-sm btn-info" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Atur Nilai" style="margin-right: 5px" ><b> Nilai</b></a>' .
                     '<a href="#" onclick="deleteModel(' . $deleteUrl . ',)" class="btn btn-sm btn-danger" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Hapus Dari Kelas" style="margin-right: 5px"><b> Hapus</b></a>' .
 
