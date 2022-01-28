f7aa2e3539f7a3f06e9aaa78096d066b01bc4130
Bali Merta <made.b4li@gmail.com>
2022-01-28 09:44:15 +0800

    Update index.blade.php

--- resources/views/ekskul/index.blade.php
+++ resources/views/ekskul/index.blade.php
@@ -9,7 +9,7 @@
             <div class="card">
                 <div class="card-header d-flex justify-content-between align-items-center">
                     <h4 class=" mb-0 ">Data Ekstrakulikuler</h4>
-                    <a href="{{ route('ekskul.create') }}" class="btn btn-primary add" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Tambah ekskul"> <i class="fas fa-folder-plus"></i> Mata Pelajaran  Baru</a>
+                    <a href="{{ route('ekskul.create') }}" class="btn btn-primary add" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Tambah ekskul"> <i class="fas fa-folder-plus"></i> Ekstrakurikuler Baru
                 </div>
                 <div class="px-3">
                     @include('layouts.flash')
