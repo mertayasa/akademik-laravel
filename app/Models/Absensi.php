e415ee9a760ec423ff131f4d70d0d8b40bf258ed
Bali Merta <made.b4li@gmail.com>
2022-03-19 10:09:05 +0800

    Fix undefined index 0 model absensi

--- app/Models/Absensi.php
+++ app/Models/Absensi.php
@@ -58,20 +58,22 @@ class Absensi extends Model
         $group_bulan = [];
         
         $absensi = $absensi->toArray();
-        $period_ganjil = \Carbon\CarbonPeriod::create(\Carbon\Carbon::parse($absensi[0]['tgl_absensi'])->firstOfMonth(), \Carbon\Carbon::parse(end($absensi)['tgl_absensi'])->endOfMonth());
-        $absensi = [];
-        $index_count = 1;
-        $last_month_name = '';
-        foreach ($period_ganjil as $dt) {
-            array_push($absensi, ['tanggal' => $dt->format('Y-m-d')]);
-            $indo_month_name = getIndonesianMonth($dt->format("Y-m"));
-            $group_bulan[$dt->format("Y-m")]['month_name'] = $indo_month_name;
-            if($indo_month_name != $last_month_name){
-                $index_count = 1;
-            }
+        if(count($absensi) > 0){
+            $period_ganjil = \Carbon\CarbonPeriod::create(\Carbon\Carbon::parse($absensi[0]['tgl_absensi'])->firstOfMonth(), \Carbon\Carbon::parse(end($absensi)['tgl_absensi'])->endOfMonth());
+            $absensi = [];
+            $index_count = 1;
+            $last_month_name = '';
+            foreach ($period_ganjil as $dt) {
+                array_push($absensi, ['tanggal' => $dt->format('Y-m-d')]);
+                $indo_month_name = getIndonesianMonth($dt->format("Y-m"));
+                $group_bulan[$dt->format("Y-m")]['month_name'] = $indo_month_name;
+                if($indo_month_name != $last_month_name){
+                    $index_count = 1;
+                }
 
-            $group_bulan[$dt->format("Y-m")]['count_absen'] = $index_count++;
-            $last_month_name = $indo_month_name;
+                $group_bulan[$dt->format("Y-m")]['count_absen'] = $index_count++;
+                $last_month_name = $indo_month_name;
+            }
         }
 
         return [
